ab065e2dd7064bc2a2241f3041508087f95d798d
lukptr <lukptr@yahoo.com>
2017-03-30 13:29:27 +0700

    modified:   Dockerfile 	deleted:    all.conf 	deleted:    setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get upgrade -y && apt-get -y autoremove && apt-get clean
-python install.py --production
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/ /home/$FRAPPE_USER/.cache
-
-exit 0
